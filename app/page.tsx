a13857a4e5a155f1da0b2c6aa0049d8839d113c8
aslobodnik <aslobodnik@gmail.com>
2026-01-24 16:50:59 -0500

    change skill grid to 2x2 layout

--- app/page.tsx
+++ app/page.tsx
@@ -130,7 +130,7 @@ export default function Home() {
 
         {/* Skill Grid */}
         <section className="mb-8">
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             {SKILLS.map((skill) => (
               <SkillCard
                 key={skill.id}
